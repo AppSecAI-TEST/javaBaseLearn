488d0a7ab8902f679f2835a85f672d19ab2907fd
xia_yu <xia_yu@Ctrip.com>
2017-04-20 11:06:30 +0800

    为Person类添加无参 全参构造方法

--- src/main/java/com/learn/pojo/Person.java
+++ src/main/java/com/learn/pojo/Person.java
@@ -1,11 +1,15 @@
 package com.learn.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by xia_yu on 2017/4/18.
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     private String name;
     private Integer age;
