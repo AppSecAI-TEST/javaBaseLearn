a5e63036b9127223326ab7b345e1c4ceab07e1ad
xiayu_lt@126.com <xiayu_lt@126.com>
2017-04-26 22:40:11 +0800

    测试xml日期

--- src/main/java/com/learn/Test/DateUtilTest.java
+++ src/main/java/com/learn/Test/DateUtilTest.java
@@ -3,7 +3,11 @@ package com.learn.Test;
 import com.learn.utils.DateUtil;
 import org.junit.Test;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by xia_yu on 2017/4/20.
@@ -20,4 +24,17 @@ public class DateUtilTest {
         Date result=DateUtil.getDateFromNow(5,DATE_PATTERN);
         System.out.println(result);
     }
+    @Test
+    public void setGregorianCalendarTest(){
+        GregorianCalendar gregorianCalendar=new GregorianCalendar();
+        gregorianCalendar.setTime(new Date());
+        XMLGregorianCalendar gc=null;
+        try {
+            gc= DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
+        System.out.println(gc.toGregorianCalendar().getTime());
+    }
+
 }
--- src/main/java/com/learn/Test/XmlSerializerTest.java
+++ src/main/java/com/learn/Test/XmlSerializerTest.java
@@ -1,7 +1,6 @@
 package com.learn.Test;
 
 
-import com.learn.serializer.User;
 import com.learn.utils.XMLUtil;
 import org.junit.Test;
 
@@ -14,13 +13,13 @@ import java.util.Date;
 public class XmlSerializerTest {
     @Test
     public void convertXml(){
-        User user=new User(1,"小明","123",new Date(),10000.0);
+        /*User user=new User(1,"小明","123",new Date(),10000.0);
         String resultXml= XMLUtil.convertToXml(user);
-        System.out.println(resultXml);
+        System.out.println(resultXml);*/
     }
     @Test
     public void convertXmlFile(){
-        User user=new User(2,"小刚","abc",new Date(),15000.0);
-        XMLUtil.convertToXml(user,"src//main//file//create.xml");
+       /* User user=new User(2,"小刚","abc",new Date(),15000.0);
+        XMLUtil.convertToXml(user,"src//main//file//create.xml");*/
     }
 }
