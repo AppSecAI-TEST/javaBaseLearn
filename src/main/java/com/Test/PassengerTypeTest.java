6db24d66b2095084775123be5e03362d904a6522
xia_yu <xia_yu@Ctrip.com>
2017-06-16 17:32:19 +0800

    添加guava依赖包

--- src/main/java/com/Test/PassengerTypeTest.java
+++ src/main/java/com/Test/PassengerTypeTest.java
@@ -3,6 +3,8 @@ package com.test;
 import com.learn.enumerate.PassengerType;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 /**
  * Created by xia_yu on 2017/4/21.
  */
@@ -11,5 +13,8 @@ public class PassengerTypeTest {
     public void getPassengerTypeCode(){
         int code= PassengerType.Adult.getValue();
         System.out.println(code);
+        BigDecimal testbig=new BigDecimal("1.55");
+        testbig=testbig.setScale(0,BigDecimal.ROUND_DOWN);
+        System.out.println(testbig.toString());
     }
 }
