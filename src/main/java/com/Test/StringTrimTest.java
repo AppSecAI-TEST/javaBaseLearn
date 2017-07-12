7e84eafb2d76de7ab2de8c46be593103d4044cbb
xia_yu <xia_yu@Ctrip.com>
2017-07-12 14:10:41 +0800

    验证字符串截取功能

--- src/main/java/com/Test/StringTrimTest.java
+++ src/main/java/com/Test/StringTrimTest.java
@@ -11,4 +11,12 @@ public class StringTrimTest {
     public void trim(){
         System.out.println(StringUtils.trim("  |验证去除前后空格|   "));
     }
+    @Test
+    public void subStr(){
+        String a="验证字符串截取功能";
+       /* a= org.apache.commons.lang3.StringUtils.abbreviate(a,8);
+        System.out.println(a);*/
+        a=a.substring(0,5);
+        System.out.println(a);
+    }
 }
